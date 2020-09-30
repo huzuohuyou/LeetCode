2632354d2cbc32047baa55cf484b946d37d3f0a7
DESKTOP-6NPNRJ4\吴海龙 <13126506430@163.com>
2020-09-30 10:27:20 +0800

    .

---
+++ 广度搜索/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace 广度搜索
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = new TreeNode(3);
+            var l1 = new TreeNode(9);
+            root.left = l1;
+            var r1 = new TreeNode(20);
+            root.right = r1;
+
+            var r1l = new TreeNode(15);
+            r1.left = r1l;
+            var r1r = new TreeNode(7);
+            r1.right = r1r;
+            var r = new Solution().LevelOrderBottom(root);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 广度搜索/Solution_107.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 广度搜索
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+    public class Solution
+    {
+        public IList<IList<int>> LevelOrderBottom(TreeNode root)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            if (root == null)
+            {
+                return result;
+            }
+            else
+            {
+                var queue = new Queue();
+                queue.Enqueue(root);
+                while (queue.Count > 0)
+                {
+                    var item = new List<int>();
+                    int size = queue.Count;
+                    for (int i = 0; i < size; i++)
+                    {
+                        var node = (TreeNode)queue.Dequeue();
+                        item.Add(node.val);
+                        if (node.left != null)
+                        {
+                            queue.Enqueue(node.left);
+                        }
+                        if (node.right != null)
+                        {
+                            queue.Enqueue(node.right);
+                        }
+                    }
+                    result.Add(item);
+                }
+                return result;
+            }
+        }
+    }
+}
