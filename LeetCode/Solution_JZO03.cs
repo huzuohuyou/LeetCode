bb07d7975b8e3393851b51b1ce3d2f4086427b76
DESKTOP-6NPNRJ4\吴海龙 <13126506430@163.com>
2020-10-12 11:13:44 +0800

    剑指Offer03

---
+++ LeetCode/Solution_JZO03.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+
+    public class Solution_JZO03 : ISolution
+    {
+        public void Test()
+        {
+
+            var x = FindRepeatNumber(new int[] { 1, 0, 3, 5, 9, 12 });
+        }
+
+        public int FindRepeatNumber(int[] nums)
+        {
+            var set = new HashSet<int>();
+            foreach (var r in nums)
+            {
+                if (!set.Contains(r))
+                {
+                    set.Add(r);
+                }
+                else
+                {
+                    return r;
+                }
+            }
+            return -1;
+            
+        }
+    }
+}
