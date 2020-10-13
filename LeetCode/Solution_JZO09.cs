92b46b343a9e207fdf75e2243fa13de869be6cf1
DESKTOP-6NPNRJ4\吴海龙 <13126506430@163.com>
2020-10-13 15:57:26 +0800

    斐波那锲的迭代思想

---
+++ LeetCode/Solution_JZO09.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+
+    public class Solution_JZO09 : ISolution
+    {
+        public void Test()
+        {
+            CQueue obj = new CQueue();
+            int param_0 = obj.DeleteHead();
+            obj.AppendTail(5);
+            obj.AppendTail(2);
+            int param_1 = obj.DeleteHead();
+            int param_2 = obj.DeleteHead();
+        }
+
+        public class CQueue
+        {
+            Stack<int> left = new Stack<int>();
+            Stack<int> right = new Stack<int>();
+            public CQueue()
+            {
+
+            }
+
+            public void AppendTail(int value)
+            {
+                if (left.Count==0)
+                {
+                    left.Push(value);
+                }
+                else
+                {
+                    while (left.Count>0)
+                    {
+                        var item = left.Pop();
+                        right.Push(item);
+                    }
+                    left.Push(value);
+                    while (right.Count>0)
+                    {
+                        var item = right.Pop();
+                        left.Push(item);
+                    }
+                }
+            }
+
+            public int DeleteHead()
+            {
+                if (left.Count>0)
+                {
+                    var item =left.Pop();
+                    return item;
+                }return -1;
+            }
+        }
+
+    }
+}
---
+++ LeetCode/Solution_JZO10.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+
+    public class Solution_JZO10 : ISolution
+    {
+        public void Test()
+        {
+            var f = Fib(41);
+        }
+
+        Dictionary<int,int> dict = new Dictionary<int, int>();
+        public int Fib(int n)
+        {
+            if (n>=0&&n<=100)
+            {
+                int f0 = 0, f1 = 1;
+                int f2 = f0+f1;
+                if (n == 0)
+                {
+                    return 0;
+                }
+                if (n == 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    for (int i = 2; i < n; i++)
+                    {
+                        f2 = (f0 + f1)%1000000007;
+                        f0 = f1;
+                        f1 = f2;
+                    }
+                    return f2;
+                }
+            }
+            return -1;
+
+        }
+    }
+}
