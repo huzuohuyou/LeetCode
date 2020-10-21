076e6085850105d3280628b93b8955a952d66056
DESKTOP-6NPNRJ4\吴海龙 <13126506430@163.com>
2020-10-21 15:58:11 +0800

    29,30

--- LeetCode/Solution_JZO29.cs
+++ LeetCode/Solution_JZO29.cs
@@ -34,48 +34,42 @@ namespace LeetCode
         List<int> result = new List<int>();
         public int[] SpiralOrder(int[][] matrix)
         {
-            while (matrix.Length > 1)
+            if (matrix.Length == 0) return new int[0];
+            int l = 0, r = matrix[0].Length - 1, t = 0, b = matrix.Length - 1, x = 0;
+            int[] res = new int[(r + 1) * (b + 1)];
+            while (true)
             {
-                for (int i = 0; i < matrix[0].Length; i++)
-                {
-                    result.Add(matrix[0][i]);
-                    if (i == matrix[0].Length - 1)
-                    {
-                        matrix = matrix.Skip(1).Take(matrix.Length - 1).ToArray();
-                        matrix = GetArray(matrix);
-                        if (matrix.Length == 1)
-                        {
-                            break;
-                        }
-                        SpiralOrder(matrix);
-                    }
-                }
+                for (int i = l; i <= r; i++) res[x++] = matrix[t][i]; // left to right.
+                if (++t > b) break;
+                for (int i = t; i <= b; i++) res[x++] = matrix[i][r]; // top to bottom.
+                if (l > --r) break;
+                for (int i = r; i >= l; i--) res[x++] = matrix[b][i]; // right to left.
+                if (t > --b) break;
+                for (int i = b; i >= t; i--) res[x++] = matrix[i][l]; // bottom to top.
+                if (++l > r) break;
             }
-            foreach (var item in matrix[0])
-            {
-                result.Add(item);
-            }
+            return res;
 
-            return result.ToArray();
+         
         }
 
 
-        public int[][] GetArray(int[][] array)
-        {
-            var temp = new int[array[0].Length][];
-            for (int j = array[0].Length - 1; j >= 0; j--)
+        //public int[][] GetArray(int[][] array)
+        //{
+        //    var temp = new int[array[0].Length][];
+        //    for (int j = array[0].Length - 1; j >= 0; j--)
 
-            {
-                var item = new int[array.Length];
-                for (int i = 0; i < array.Length; i++)
-                {
-                    item[i] = array[i][j];
-                }
-                temp[array[0].Length - j - 1] = item;
-            }
-            return temp;
+        //    {
+        //        var item = new int[array.Length];
+        //        for (int i = 0; i < array.Length; i++)
+        //        {
+        //            item[i] = array[i][j];
+        //        }
+        //        temp[array[0].Length - j - 1] = item;
+        //    }
+        //    return temp;
 
-        }
+        //}
 
 
     }
---
+++ LeetCode/Solution_JZO30.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+
+    public class Solution_JZO30 : ISolution
+    {
+
+        public void Test()
+        {
+            MinStack minStack = new MinStack();
+            minStack.Push(-2);
+            minStack.Push(0);
+            minStack.Push(-3);
+            var c = minStack.Min();
+            minStack.Pop();
+            var a = minStack.Top();
+            var b = minStack.Min();
+
+        }
+        public class MinStack
+        {
+            public Stack<int> A = new Stack<int>();
+            public Stack<int> B = new Stack<int>();
+            /** initialize your data structure here. */
+            public MinStack()
+            {
+
+            }
+
+            public void Push(int x)
+            {
+                A.Push(x);
+                if (B.Count==0||x<=B.Peek())
+                {
+                    B.Push(x);
+                }
+            }
+
+            public void Pop()
+            {
+                if (A.Pop().Equals(B.Peek()))
+                {
+                    B.Pop();
+                }
+            }
+
+            public int Top()
+            {
+                
+                return A.Peek();
+            }
+
+            public int Min()
+            {
+                if (B.Count > 0)
+                {
+                    return  B.Peek();
+                }return A.Peek();
+                
+            }
+        }
+    }
+}
+
