fd8e1c2aa56db8bee77f11e535cdd76a98b6a9c3
DESKTOP-6NPNRJ4\吴海龙 <13126506430@163.com>
2020-10-22 16:59:58 +0800

    ，

---
+++ LeetCode/Solution_JZO32.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+
+    public class Solution_JZO32 : ISolution
+    {
+
+        public void Test()
+        {
+           
+
+        }
+
+
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            var result = new List<IList<int>>();
+            if (root == null)
+            {
+                return result;
+            }
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                var item = new List<int>();
+                var size = queue.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    var node = queue.Dequeue();
+                    item.Add(node.val);
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+                result.Add(item);
+            }
+            return result;
+        }
+    }
+}
+
---
+++ LeetCode/Solution_JZO39.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+
+    public class Solution_JZO39 : ISolution
+    {
+        public void Test()
+        {
+           
+
+        }
+
+
+        public int MajorityElement(int[] nums)
+        {
+            int vote = 0, x = 0;
+            foreach (var item in nums)
+            {
+                if (vote==0)
+                {
+                    x = item;
+                }
+                vote += item == x ? 1 : -1;
+            }
+            return x;
+        }
+    }
+}
+
