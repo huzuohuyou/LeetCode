24b9324bc1368e7b126eaf2ccf0cda3876a6ffdb
DESKTOP-6NPNRJ4\吴海龙 <13126506430@163.com>
2020-10-26 15:32:33 +0800

    54,55

---
+++ LeetCode/Solution_JZO54.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+
+    public class Solution_JZO54 : ISolution
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+        public void Test()
+        {
+            var root = new TreeNode(5);
+            var l1 = new TreeNode(3);
+            root.left = l1;
+            var r1 = new TreeNode(6);
+            root.right = r1;
+
+            var r1l = new TreeNode(2);
+            l1.left = r1l;
+            var r1r = new TreeNode(4);
+            l1.right = r1r;
+
+            var r2r = new TreeNode(1);
+            r1l.left = r2r;
+            var a = KthLargest(root,3);
+        }
+
+        List<TreeNode> list = new List<TreeNode>();
+        public void Dfs(TreeNode root) {
+            if (root==null)
+            {
+                return;
+            }
+            if (root.left==null&&root.right==null)
+            {
+                list.Add(root);
+
+            }
+            else
+            {
+                if (root.right != null)
+                {
+                    Dfs(root.right);
+                }
+                list.Add(root);
+                if (root.left!=null)
+                {
+                    Dfs(root.left);
+                }
+                
+            }
+
+        }
+
+        public int KthLargest(TreeNode root, int k)
+        {
+            Dfs(root);
+            return list[k - 1].val;
+        }
+    }
+}
+
---
+++ LeetCode/Solution_JZO55_1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+
+    public class Solution_JZO55_1 : ISolution
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+        public void Test()
+        {
+            var root = new TreeNode(5);
+            var l1 = new TreeNode(3);
+            root.left = l1;
+            var r1 = new TreeNode(6);
+            root.right = r1;
+
+            var r1l = new TreeNode(2);
+            l1.left = r1l;
+            var r1r = new TreeNode(4);
+            l1.right = r1r;
+
+            var r2r = new TreeNode(1);
+            r1l.left = r2r;
+            var a = MaxDepth(root);
+        }
+
+        int Deph=0;
+        
+
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            if (root.left == null && root.right == null)
+            {
+                return 1;
+
+            }
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count>0)
+            {
+                var size = queue.Count;
+                
+                Deph++;
+                for (int i = 0; i < size; i++)
+                {
+                    var node = queue.Dequeue();
+                    if (node.left!=null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right!=null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+                
+            }
+            return Deph;
+        }
+    }
+}
+
