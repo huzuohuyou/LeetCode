02f82a4b6362664f1dff44ea827c5439981413b9
DESKTOP-6NPNRJ4\吴海龙 <13126506430@163.com>
2020-10-27 14:22:36 +0800

    57

---
+++ LeetCode/Solution_JZO57_1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+
+    public class Solution_JZO57_1 : ISolution
+    {
+        
+        public void Test()
+        {
+           
+            var a = TwoSum(new int[] { 2, 7, 11, 15 },9);
+        }
+
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int start = 0, end = nums.Length - 1;
+            var array = new int[2];
+            while (start<end)
+            {
+                if (nums[start]+nums[end]>target)
+                {
+                    end--;
+                }
+                else if (nums[start] + nums[end]< target)
+                {
+                    start++;
+                }else
+                {
+                    array[0] = nums[start];
+                    array[1] = nums[end];
+                    break;
+                }
+            }
+            return array;
+            
+        }
+    }
+}
+
---
+++ LeetCode/Solution_JZO57_2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+
+    public class Solution_JZO57_2 : ISolution
+    {
+        
+        public void Test()
+        {
+           
+            var a = FindContinuousSequence(15);
+        }
+
+
+        public int[][] FindContinuousSequence(int target)
+        {
+            var result = new List<int[]>();
+            for (int i = 1,j=2;  i< j; )
+            {
+                var sum = (i + j) * (j - i + 1) / 2;
+                if (sum==target)
+                {
+                    var list = new List<int>();
+                    for (int m = i; m <= j - i + 1; m++)
+                    {
+                        list.Add(m);
+                    }
+                    result.Add(list.ToArray());
+                    i++;
+                }
+                else if (sum<target)
+                {
+                    j++;
+                }
+                else if (sum>target)
+                {
+                    i++;
+                }
+            }
+            return result.ToArray();
+        }
+    }
+}
+
