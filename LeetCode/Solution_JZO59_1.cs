538c985c60f73eccc20693a2c56688b0dd59a004
DESKTOP-6NPNRJ4\吴海龙 <13126506430@163.com>
2020-10-28 10:46:37 +0800

    59

---
+++ LeetCode/Solution_JZO59_1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+
+    public class Solution_JZO59_1 : ISolution
+    {
+        
+        public void Test()
+        {
+           
+            var a = MaxSlidingWindow(new int[] { 1, 3, -1, -3, 5, 3, 6, 7 },3);
+        }
+
+
+        public int[] MaxSlidingWindow(int[] nums, int k)
+        {
+            if (k > nums.Length || nums.Length == 0)
+            {
+                return nums;
+            }
+            var array = new int[nums.Length - k + 1];
+            for (int l = 0, r = l + k - 1; l < nums.Length - k + 1; l++)
+            {
+                array[l] = nums.Skip(l).Take(k).ToArray().Max();
+            }
+            return array;
+        }
+    }
+}
+
