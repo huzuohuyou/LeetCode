f46d3de1f18916051cd75766b02354c00d223220
HailongWu <13126506430@163.com>
2021-06-24 09:43:48 +0800

    20、68

--- LeetCode/Solution_JZO20.cs
+++ LeetCode/Solution_JZO20.cs
@@ -6,7 +6,9 @@ using System.Text;
 
 namespace LeetCode
 {
-
+    /// <summary>
+    /// 剑指 Offer 20. 表示数值的字符串
+    /// </summary>
     public class Solution_JZO20 : ISolution
     {
         public class ListNode
@@ -17,51 +19,137 @@ namespace LeetCode
         }
         public void Test()
         {
-            var n1 = new ListNode(4);
-            var n2 = new ListNode(5);
-            n1.next = n2;
-
-            var n3 = new ListNode(1);
-            n2.next = n3;
-
-            var n4 = new ListNode(9);
-            n3.next = n4;
+           
 
            
-            var f = DeleteNode(n1,4);
         }
-        public ListNode DeleteNode(ListNode head, int val)
+        public bool IsNumber(string s)
         {
-            
-            var stack = new Stack<ListNode>();
-            while (head!=null)
+            Dictionary<State, Dictionary<CharType, State>> transfer = new Dictionary<State, Dictionary<CharType, State>>();
+            Dictionary<CharType, State> initialDictionary = new Dictionary<CharType, State> {
+            {CharType.CHAR_SPACE, State.STATE_INITIAL},
+            {CharType.CHAR_NUMBER, State.STATE_INTEGER},
+            {CharType.CHAR_POINT, State.STATE_POINT_WITHOUT_INT},
+            {CharType.CHAR_SIGN, State.STATE_INT_SIGN}
+        };
+            transfer.Add(State.STATE_INITIAL, initialDictionary);
+            Dictionary<CharType, State> intSignDictionary = new Dictionary<CharType, State> {
+            {CharType.CHAR_NUMBER, State.STATE_INTEGER},
+            {CharType.CHAR_POINT, State.STATE_POINT_WITHOUT_INT}
+        };
+            transfer.Add(State.STATE_INT_SIGN, intSignDictionary);
+            Dictionary<CharType, State> integerDictionary = new Dictionary<CharType, State> {
+            {CharType.CHAR_NUMBER, State.STATE_INTEGER},
+            {CharType.CHAR_EXP, State.STATE_EXP},
+            {CharType.CHAR_POINT, State.STATE_POINT},
+            {CharType.CHAR_SPACE, State.STATE_END}
+        };
+            transfer.Add(State.STATE_INTEGER, integerDictionary);
+            Dictionary<CharType, State> pointDictionary = new Dictionary<CharType, State> {
+            {CharType.CHAR_NUMBER, State.STATE_FRACTION},
+            {CharType.CHAR_EXP, State.STATE_EXP},
+            {CharType.CHAR_SPACE, State.STATE_END}
+        };
+            transfer.Add(State.STATE_POINT, pointDictionary);
+            Dictionary<CharType, State> pointWithoutIntDictionary = new Dictionary<CharType, State> {
+            {CharType.CHAR_NUMBER, State.STATE_FRACTION}
+        };
+            transfer.Add(State.STATE_POINT_WITHOUT_INT, pointWithoutIntDictionary);
+            Dictionary<CharType, State> fractionDictionary = new Dictionary<CharType, State> {
+            {CharType.CHAR_NUMBER, State.STATE_FRACTION},
+            {CharType.CHAR_EXP, State.STATE_EXP},
+            {CharType.CHAR_SPACE, State.STATE_END}
+        };
+            transfer.Add(State.STATE_FRACTION, fractionDictionary);
+            Dictionary<CharType, State> expDictionary = new Dictionary<CharType, State> {
+            {CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER},
+            {CharType.CHAR_SIGN, State.STATE_EXP_SIGN}
+        };
+            transfer.Add(State.STATE_EXP, expDictionary);
+            Dictionary<CharType, State> expSignDictionary = new Dictionary<CharType, State> {
+            {CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER}
+        };
+            transfer.Add(State.STATE_EXP_SIGN, expSignDictionary);
+            Dictionary<CharType, State> expNumberDictionary = new Dictionary<CharType, State> {
+            {CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER},
+            {CharType.CHAR_SPACE, State.STATE_END}
+        };
+            transfer.Add(State.STATE_EXP_NUMBER, expNumberDictionary);
+            Dictionary<CharType, State> endDictionary = new Dictionary<CharType, State>() {
+            {CharType.CHAR_SPACE, State.STATE_END}
+        };
+            transfer.Add(State.STATE_END, endDictionary);
+
+            int length = s.Length;
+            State state = State.STATE_INITIAL;
+
+            for (int i = 0; i < length; i++)
             {
-                if (val==head.val)
+                CharType type = ToCharType(s[i]);
+                if (!transfer[state].ContainsKey(type))
                 {
-                    if (stack.Count > 0 )
-                    {
-                        var pre = stack.Pop();
-                        pre.next = head.next;
-                        head = pre;
-                        break;
-                    }
-                    else
-                    {
-                        return head.next;
-                    }
-                    
+                    return false;
                 }
                 else
                 {
-                    stack.Push(head);
+                    state = transfer[state][type];
                 }
-                
-                head = head.next;
-                
             }
-
-            return stack.Count > 0 ? stack.Last() : head ;
+            return state == State.STATE_INTEGER || state == State.STATE_POINT || state == State.STATE_FRACTION || state == State.STATE_EXP_NUMBER || state == State.STATE_END;
         }
+
+        CharType ToCharType(char ch)
+        {
+            if (ch >= '0' && ch <= '9')
+            {
+                return CharType.CHAR_NUMBER;
+            }
+            else if (ch == 'e' || ch == 'E')
+            {
+                return CharType.CHAR_EXP;
+            }
+            else if (ch == '.')
+            {
+                return CharType.CHAR_POINT;
+            }
+            else if (ch == '+' || ch == '-')
+            {
+                return CharType.CHAR_SIGN;
+            }
+            else if (ch == ' ')
+            {
+                return CharType.CHAR_SPACE;
+            }
+            else
+            {
+                return CharType.CHAR_ILLEGAL;
+            }
+        }
+
+        enum State
+        {
+            STATE_INITIAL,
+            STATE_INT_SIGN,
+            STATE_INTEGER,
+            STATE_POINT,
+            STATE_POINT_WITHOUT_INT,
+            STATE_FRACTION,
+            STATE_EXP,
+            STATE_EXP_SIGN,
+            STATE_EXP_NUMBER,
+            STATE_END
+        }
+
+        enum CharType
+        {
+            CHAR_NUMBER,
+            CHAR_EXP,
+            CHAR_POINT,
+            CHAR_SIGN,
+            CHAR_SPACE,
+            CHAR_ILLEGAL
+        }
+
     }
 }
 
---
+++ LeetCode/Solution_JZO68_2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+
+    public class Solution_JZO68_2 : ISolution
+    {
+
+        public void Test()
+        {
+
+            //var a = Add(1,2);
+        }
+
+
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (root == null || root == p || root == q) return root;
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+            if (left == null) return right;
+            if (right == null) return left;
+            return root;
+        }
+
+ 
+    }
+}
+
