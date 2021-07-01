d33a91f4c3ae1e84a208b1471a36e74cb2ef31b1
HailongWu <13126506430@163.com>
2021-07-01 10:03:47 +0800

    剑指 Offer 63. 股票的最大利润 动态规划

---
+++ LeetCode/Solution_JZO63.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    /// <summary>
+    /// 剑指 Offer 63. 股票的最大利润
+    /// https://leetcode-cn.com/problems/gu-piao-de-zui-da-li-run-lcof/
+    /// </summary>
+    public class Solution_JZO63 : ISolution
+    {
+
+        public void Test()
+        {
+
+            var a = MaxProfit(new int[] { 4, 7, 5, 2, 9 });
+        }
+
+
+        public int MaxProfit(int[] prices)
+        {
+
+            if (prices.Length<=1)
+            {
+                return 0;
+            }
+            int cost = int.MaxValue, profit = 0;
+            foreach (var price in prices)
+            {
+                cost = Math.Min(cost, price);
+                profit = Math.Max(profit, price - cost);
+            }
+            return profit;
+
+     
+        }
+    }
+}
+
