2c3c818dd0892749a2bbd66d8e19bd43462b71ba
HailongWu <13126506430@163.com>
2021-07-02 09:47:44 +0800

    短路，递归

---
+++ LeetCode/Solution_JZO64.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    /// <summary>
+    /// 剑指 Offer 64. 求1+2+…+n
+    /// https://leetcode-cn.com/problems/qiu-12n-lcof/
+    /// </summary>
+    public class Solution_JZO64 : ISolution
+    {
+
+        public void Test()
+        {
+
+            var a = SumNums(7);
+        }
+
+
+        public int SumNums(int n)
+        {
+            bool x = n > 1 && (n += SumNums(n - 1)) > 0;
+            return n;
+        }
+    }
+}
+
